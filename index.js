6ee2ce422c19ea4369d042487ae0088a35fe4a7e
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-09 13:33:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Your code goes here
 
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
-  changeText()
-  
-});
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
 
-function changeText(){
- text = document.getElementsById('text')
- text.innerHTML='This is really cool'
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
 }
