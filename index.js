cc289fdd02aac61eccc404000d7e8db58f5c2805
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-09 13:22:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Your code goes here
 
-document.addEventListener("DOMContentLoaded",)
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  
+});
+
+document.getElementsById('text')
